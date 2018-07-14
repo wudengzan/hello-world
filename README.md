2399e020d35c57e07b95953798ca194b38904a5e
root <root@ip-172-31-39-128.us-east-2.compute.internal>
2018-07-14 11:49:15 +0000

    ii

--- README.md
+++
@@ -1,2 +0,0 @@
-# hello-world
-print 566
--- app.js
+++
@@ -1,5 +0,0 @@
-var app = require("http");//çfirst,import htttp 
-app.createServer(function(request,response){
-response.writeHead(200,{"content-type":"text/plain"});
-response.end("hello");}).listen(80);
-console.log("running server");
